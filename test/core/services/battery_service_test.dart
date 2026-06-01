e160eeca2b9a5e0204d50980a679e1887955eed4
hardikp <hardikpatel679@gmail.com>
2026-06-01 11:51:54 +0530

    update Jenkinsfile

--- test/core/services/battery_service_test.dart
+++ test/core/services/battery_service_test.dart
@@ -2,6 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:core/services/battery_service.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
   late BatteryService batteryService;
 
   setUp(() {
